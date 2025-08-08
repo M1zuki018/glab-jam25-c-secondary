52412b087e5167a341f81c003dda850389e43050
ayato0913a <ayato0913a@gmail.com>
2025-08-08 17:26:24 +0900

    [update] Textだけ動くように変更

--- Assets/yamada/Scripts/StoryMove.cs
+++ Assets/yamada/Scripts/StoryMove.cs
@@ -4,16 +4,16 @@ using UnityEngine.UI;
 
 public class StoryMove : MonoBehaviour
 {
-    [SerializeField] private Image _storyImage;
+    [SerializeField] private Text _storyText;
     [SerializeField] private float _moveCoordinate = 20f;
     [SerializeField] private float _moveDuration = 10f;
     private Tween _moveStoryTween;
 
     void Start()
     {
-        RectTransform _moveStoryImage = _storyImage.rectTransform;
+        RectTransform _moveStoryText = _storyText.rectTransform;
 
-        _moveStoryTween = _moveStoryImage.DOAnchorPosY(_moveCoordinate, _moveDuration)
+        _moveStoryTween = _moveStoryText.DOAnchorPosY(_moveCoordinate, _moveDuration)
             .SetEase(Ease.Linear)
             .OnComplete(() =>
             {
